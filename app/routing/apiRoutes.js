0e5a71b60e3fdbaf931f57f98a64d6097046d359
Diana Mayhugh <dianamayhugh@gmail.com>
2020-06-27 14:38:16 -0400

    initialize htmls and post requests

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,8 +4,7 @@
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
 
-var tableData = require("../data/tableData");
-var waitListData = require("../data/waitinglistData");
+var friendsData = require("../data/friends");
 
 // ===============================================================================
 // ROUTING
@@ -18,12 +17,8 @@ module.exports = function (app) {
   // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
 
-  app.get("/api/tables", function (req, res) {
-    res.json(tableData);
-  });
-
-  app.get("/api/waitlist", function (req, res) {
-    res.json(waitListData);
+  app.get("/api/friends", function (req, res) {
+    res.json(friendsData);
   });
 
   // API POST Requests
@@ -34,7 +29,7 @@ module.exports = function (app) {
   // Then the server saves the data to the tableData array)
   // ---------------------------------------------------------------------------
 
-  app.post("/api/tables", function (req, res) {
+  app.post("/api/friends", function (req, res) {
     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
     // It will do this by sending out the value "true" have a table
     // req.body is available since we're using the body parsing middleware
