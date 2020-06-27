e33dca52f668739fca7e31c85dec6775f5d8c4e9
Diana Mayhugh <dianamayhugh@gmail.com>
2020-06-27 10:27:25 -0400

    set up server.js

--- server.js
+++ server.js
@@ -3,14 +3,24 @@
 // Series of npm packages that we will use to give our server useful functionality
 // ==============================================================================
 
-
+let express = require("express");
+let path = require("path");
+let { response } = require("express");
 
 // ==============================================================================
 // EXPRESS CONFIGURATION
 // This sets up the basic properties for our express server
 // ==============================================================================
 
+//Tells node that we are creating an "express" server
+let app = express();
 
+//Sets an initial port. We'll use later in our listener
+let PORT = 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // ================================================================================
 // ROUTER
@@ -18,11 +28,14 @@
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
 
-
-
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 
 // =============================================================================
 // LISTENER
 // The below code effectively "starts" our server
 // =============================================================================
+app.listen(port, function () {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
