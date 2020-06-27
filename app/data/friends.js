90a82e6ca2f3cfd9c7baa06a80c6dd51a3faa50c
Diana Mayhugh <dianamayhugh@gmail.com>
2020-06-27 14:43:49 -0400

    dummy array in friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,28 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the friends' info.
+// Initially we just set it equal to a "dummy" friend.
+// But you could have it be an empty array as well.
+// ===============================================================================
+
+var friendArray = [
+  {
+    name: "Diana",
+    photo: ,
+    scores: [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+    ]
+  },
+];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = tableArray;
