5806e60f7a546a2a9b7e9b044a676746a4b14902
Diana Mayhugh <dianamayhugh@gmail.com>
2020-06-29 15:07:35 -0400

    logic for friends comparison

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,62 +3,50 @@
 // We are linking our routes to a series of "data" sources.
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
-
-var friendsData = require("../data/friends");
-
+var friendsData = require("../data/friends.js");
 // ===============================================================================
 // ROUTING
 // ===============================================================================
-
 module.exports = function (app) {
   // API GET Requests
   // Below code handles when users "visit" a page.
   // In each of the below cases when a user visits a link
   // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
-
   app.get("/api/friends", function (req, res) {
     res.json(friendsData);
   });
-
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
-
   app.post("/api/friends", function (req, res) {
     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
     // It will do this by sending out the value "true" have a table
     // req.body is available since we're using the body parsing middleware
-    var newPerson = req.body;
-
-    //var newPersonTotalScore = 10;
-    //var lowestDifference = 2;
-    //var lowestMatch = "lena"
-
-
+    var newFriendScores = req.body.scores;
+    var scoresArray = [];
+    var friendCount = 0;
+    var bestMatch = 0;
     for (let i = 0; i < friendsData.length; i++) {
-      const element = friendsData[i];
-      const elementTotalScore = 8;
-      console.log(element);
-      
-      //if (friendsData)
+      var scoresDiff = 0;
+      //run through scores to compare friends
+      for (var j = 0; j < newFriendScores.length; j++) {
+        scoresDiff += Math.abs(
+          parseInt(friendsData[i].scores[j]) - parseInt(newFriendScores[j])
+        );
+      }
+      //push results into scoresArray
+      scoresArray.push(scoresDiff);
     }
-    res.send("Hello World");
-  });
-
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
-
-  app.post("/api/clear", function (req, res) {
-    // Empty out the arrays of data
-    tableData.length = 0;
-    waitListData.length = 0;
-
-    res.json({ ok: true });
+    //after all friends are compared, find best match
+    for (var i = 0; i < scoresArray.length; i++) {
+      if (scoresArray[i] <= scoresArray[bestMatch]) {
+        bestMatch = i;
+      }
+    }
+    //return bestMatch data
+    var bff = friendsData[bestMatch];
+    res.json(bff);
+    //pushes new submission into the friendsdata array
+    friendsData.push(req.body);
   });
 };
+
+// res.json({ ok: true });
